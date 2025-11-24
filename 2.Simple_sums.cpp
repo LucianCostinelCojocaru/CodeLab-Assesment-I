6c6c56a9b8e5056610b47abeafcde55d463d88c2
LucianCostinelCojocaru <clucian1234@gmail.com>
2025-11-24 03:12:13 +0000

    Add files via upload

---
+++ 2.Simple_sums.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long a = 8;   // fixed value
+    long b = 10;  // fixed value
+
+    cout << a + b << endl;  // prints 18
+    return 0;
+}
+
+
